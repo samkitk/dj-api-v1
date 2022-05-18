5fb6d3d46712588fa22bb13d2e08586ead43427b
Samkit <28357583+samkitk@users.noreply.github.com>
2022-05-18 13:32:37 +0530

    chore: created views for app

--- t0d0/todo_app/views.py
+++ t0d0/todo_app/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .serializer import ToDoSerializer
+from .models import ToDo
 
-# Create your views here.
+class ListToDo(generics.ListAPIView):
+    queryset = ToDo.objects.all()
+    serializer_class = ToDoSerializer
+    
+class DetailToDo(generics.RetrieveAPIView):
+    queryset = ToDo.objects.all()
+    serializer_class = ToDoSerializer
+    
+class AddToDo(generics.CreateAPIView):
+    queryset = ToDo.objects.all()
+    serializer_class = ToDoSerializer
+    
